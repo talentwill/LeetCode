d505917fd047a12e4d6ca9b1d08665d90547cd1b
talentwill <talentwill@outlook.com>
2023-08-25 21:38:54 +0800

    02.08 环路检测

---
+++ study-plans/cracking-the-coding-interview/02.08_Linked List Cycle/detectCycle.py
@@ -0,0 +1,15 @@
+# 2023-08-20
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        fast = slow = head
+        while fast:
+            if not fast.next:
+                return None
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                while head != slow:
+                    head = head.next
+                    slow = slow.next
+                return slow
+        return None
