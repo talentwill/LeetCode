20c484d770199ed50b468c5ec01e4cb49c00a472
talentwill <talentwill@outlook.com>
2023-08-25 21:38:46 +0800

    02.07 链表相交

---
+++ study-plans/cracking-the-coding-interview/02.07_Intersection of Two Linked Lists/getIntersectionNode.py
@@ -0,0 +1,8 @@
+# 2023-08-20
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        l1, l2 = headA, headB
+        while l1 != l2:
+            l1 = l1.next if l1 else headB
+            l2 = l2.next if l2 else headA
+        return l1
